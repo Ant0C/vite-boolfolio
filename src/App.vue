08b95c5937c6db2852998d3f34d60cc3dd57090f
Ant0C <cafeo97@gmail.com>
2023-05-02 16:03:47 +0200

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <script>
+import Projects from './components/Projects.vue'
 
 export default {
-
+  components: {
+    Projects
+  }
 }
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <Projects></Projects>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
---
+++ src/components/Projects.vue
@@ -0,0 +1,37 @@
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then(res => {
+                    console.log(res)
+                    const { projects } = res.data
+                    this.projects = projects
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    },
+    mounted() {
+        this.fetchProjects()
+    }
+}
+</script>
+
+<template>
+    <div>
+        <p v-for="item in projects" :key="item.id">
+            {{ item.name }}
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
